3aa1d7869e5c123b6e5b2bd8cf2e4cdd04d40fa3
konrad <39109911+Lord-KD@users.noreply.github.com>
2019-04-09 19:50:30 +0800

    十进制转二进制，页面基本功能完成

---
+++ js/main.js
@@ -0,0 +1,50 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        my_input : null,
+        is_true: false,
+        result: false,
+        is_answer: false,
+        numD: null,
+        numB: null,
+        input_nums : []
+    },
+    computed: {
+        randomNumD: function () {
+            return this.numD = Math.ceil(Math.random()*10)
+        },
+        randomNumB: function () {
+            return this.numB = this.numD.toString(2)
+        }
+    },
+    methods:{
+        submitted: function () {
+            this.result = true;
+
+            if (this.input_nums.length) {
+                this.input_nums = this.my_input.split('');
+            }
+
+            if (this.my_input === this.numB){
+                this.is_true = true
+            }else {
+                this.is_true = false
+            }
+
+        },
+        reload: function () {
+            location.reload()
+        },
+        answer: function () {
+            this.is_answer = !this.is_answer
+        },
+        red_or_green: function (item,index) {
+            answer_nums = this.numB.split('');
+
+            if (index < answer_nums.length && answer_nums[index] === item){
+                return 'green'
+            }
+            return 'red'
+        }
+    }
+});
